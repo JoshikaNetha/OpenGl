b34068f0f1c14e3d55c251347401ac4f2bef8276
joshika.namani <joshika.namani@braneenterprises.com>
2024-10-11 18:41:02 +0530

    Created Basic Winidow

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -1,23 +1,22 @@
-#include<iostream>
-using namespace std;
-
-#include <glad/glad.h>  
-/*before glfw The include file for GLAD includes the required OpenGL headers behind the scenes 
-(like GL/gl.h) so be sure to include GLAD before other header files that require OpenGL (like GLFW). */
-#include <GLFW/glfw3.h>
+#include <iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 int main(){
-
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
-    
-    GLFWwindow* window = glfwCreateWindow(400,300,"BasicWindow",NULL,NULL);
-    if(window==NULL){
+    GLFWwindow* window = glfwCreateWindow(500,500,"Basic Window",NULL,NULL);
+    if(window == NULL){
+        std::cout<<"Failed to Create Window"<<std::endl;
         glfwTerminate();
-        return 0;
-    } 
+        return -1;
+    }
     glfwMakeContextCurrent(window);
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        
+    }
 
     glfwTerminate();
     return 0;
