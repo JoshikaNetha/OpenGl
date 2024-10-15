89bf16cd9e440b081d7ce017b4b2ac7d3ea4ae28
joshika.namani <joshika.namani@braneenterprises.com>
2024-10-15 19:26:42 +0530

    Integrated 3D Model

---
+++ src/shaders/Texture.h
@@ -0,0 +1,21 @@
+#ifndef TEXTURE_CLASS_h
+#define TEXTURE_CLASS_h
+#include <glad/glad.h>
+#include <stb_image.h>
+#include"ShaderClass.h"
+class Texture{
+    public:
+   	GLuint ID;
+	GLenum type;
+	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum pixelType);
+
+	// Assigns a texture unit to a texture
+	void texUnit(Shader& shader, const char* uniform, GLuint unit);
+	// Binds a texture
+	void Bind();
+	// Unbinds a texture
+	void Unbind();
+	// Deletes a texture
+	void Delete();
+};
+#endif
